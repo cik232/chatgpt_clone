702655ca7dba15def2a4ecd0fdbaf2270f573976
cik232 <azamjon23202@gmail.com>
2024-02-22 19:12:42 +0500

    finish

---
+++ app/src/main/java/com/bdtopcoder/chatgpt/API.java
@@ -0,0 +1,6 @@
+package com.bdtopcoder.chatgpt;
+
+public class API {
+    public static String API_URL = "https://api.openai.com/v1/completions";
+    public static String API = "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";
+}
